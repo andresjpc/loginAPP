7e0459d9870cbcd196794940e656c19557375561
andresjpc <andresjpc@hotmail.com>
2018-06-02 06:35:05 -0400

    Creación de Servicio AUTH y registro de usuarios

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCZ1gRQUTOr5m8o4v7AfX_xrTTIjHGNvXA',
+    authDomain: 'pruebas-6da6b.firebaseapp.com',
+    databaseURL: 'https://pruebas-6da6b.firebaseio.com',
+    projectId: 'pruebas-6da6b',
+    storageBucket: 'pruebas-6da6b.appspot.com',
+    messagingSenderId: '172471732626'
+
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCZ1gRQUTOr5m8o4v7AfX_xrTTIjHGNvXA',
+    authDomain: 'pruebas-6da6b.firebaseapp.com',
+    databaseURL: 'https://pruebas-6da6b.firebaseio.com',
+    projectId: 'pruebas-6da6b',
+    storageBucket: 'pruebas-6da6b.appspot.com',
+    messagingSenderId: '172471732626'
+
+  }
 };
 
 /*
